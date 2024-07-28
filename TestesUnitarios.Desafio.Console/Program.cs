b80faac20c8953269e833945f25873b5b48f4330
Deyvson <deyvsonxts@gmail.com>
2024-07-28 19:32:26 -0300

    Desafio concluido

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestesUnitarios.Desafio.Console.Services;
+
 Console.WriteLine("Hello, World!");
+
+List<int> Lista = new List<int>();
+
+Lista.Add (1);
+Lista.Add (3);
+Lista.Add (5);
+
+ValidacoesLista Validacoes = new ValidacoesLista();
+
+List<int> ListaMultiplicada=Validacoes.MultiplicarNumerosLista(Lista, 5);
+
+Console.WriteLine(ListaMultiplicada);
